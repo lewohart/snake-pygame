7eb8e0071c41eb7cd9cff926607282b80e8ce4d2
lewohart <leonardo.gomesfrancisco@logmein.com>
2020-06-24 20:28:40 -0400

    Migrate to python_boilerplate

---
+++ src/snake_pygame/tests/basic_test.py
@@ -0,0 +1,13 @@
+'''
+snake-pygame: Test module.
+
+Meant for use with py.test.
+Write each test as a function named test_<something>.
+Read more here: http://pytest.org/
+
+Copyright 2020, Leonardo Gomes-Francisco
+Licensed under MIT
+'''
+
+def test_example():
+    assert True
